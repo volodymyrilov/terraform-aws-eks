d122d19d45e00dfe206763e1e3b945279d89ef6c
Bohdan Yurov <bohdan@yurov.me>
2020-04-30 12:39:16 +0300

    IAM fix

--- cluster.tf
+++ cluster.tf
@@ -92,6 +92,6 @@ resource "aws_iam_policy" "cluster_additional" {
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_additional" {
-  role       = "${aws_iam_role.cluster.arn}"
+  role       = "${aws_iam_role.cluster.name}"
   policy_arn = "${aws_iam_policy.cluster_additional.arn}"
 }

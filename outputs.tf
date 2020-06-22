852a996f8fdd7f42b8ada7869a9c4085cddbffd3
Denys Yermakov <ermakov009@gmail.com>
2020-06-22 15:26:17 +0300

    add

--- outputs.tf
+++ outputs.tf
@@ -113,7 +113,7 @@ output "worker_iam_role_arn" {
   value       = "${element(coalescelist(aws_iam_role.workers.*.arn, data.aws_iam_instance_profile.custom_worker_group_iam_instance_profile.*.role_arn, data.aws_iam_instance_profile.custom_worker_group_launch_template_iam_instance_profile.*.role_arn, data.aws_iam_instance_profile.custom_worker_group_launch_template_mixed_iam_instance_profile.*.role_arn), 0)}"
 }
 
-output "oidc_provider_arn" {
-  description = "The ARN of the OIDC Provider if `enable_irsa = true`."
-  value       = "${element(coalescelist(aws_iam_openid_connect_provider.oidc_provider.*.arn), 0)}"
-}
+// output "oidc_provider_arn" {
+//   description = "The ARN of the OIDC Provider if `enable_irsa = true`."
+//   value       = "${element(coalescelist(aws_iam_openid_connect_provider.oidc_provider.*.arn), 0)}"
+// }

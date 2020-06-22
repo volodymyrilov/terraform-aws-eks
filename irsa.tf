8094c3c6ea473546bebd8ee7b8a8f66fea7521f0
Denys Yermakov <ermakov009@gmail.com>
2020-06-22 16:09:49 +0300

    add

--- irsa.tf
+++ irsa.tf
@@ -2,5 +2,9 @@ resource "aws_iam_openid_connect_provider" "oidc_provider" {
   count           = "${var.enable_irsa ? 1 : 0}"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["${var.eks_oidc_root_ca_thumbprint}"]
-  url             = "${aws_eks_cluster.this.identity.0.oidc.0.issuer}"
+  url             = "${data.aws_eks_cluster.example.identity.0.oidc.0.issuer}"
+}
+
+data "aws_eks_cluster" "example" {
+  name = "${aws_eks_cluster.this.name}"
 }

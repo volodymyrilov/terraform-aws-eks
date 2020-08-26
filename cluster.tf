19bdc760a9f4af2318050cf4ecae9cc46c852075
Oleksandr Shkovyra <oleksandr.shkovyra@gmail.com>
2020-08-26 15:49:11 +0300

    add fixes

--- cluster.tf
+++ cluster.tf
@@ -52,6 +52,17 @@ resource "aws_security_group_rule" "cluster_https_worker_ingress" {
   type                     = "ingress"
 }
 
+resource "aws_security_group_rule" "cluster_https_worker_ingress" {
+  count                    = "${var.cluster_create_security_group ? 1 : 0}"
+  description              = "Allow pods to communicate with the EKS cluster API."
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.cluster.id}"
+  from_port                = 443
+  to_port                  = 443
+  type                     = "ingress"
+  cidr_blocks              = "${var.cluster_sg_additional_networks}"
+}
+
 resource "aws_iam_role" "cluster" {
   count                 = "${var.manage_cluster_iam_resources ? 1 : 0}"
   name_prefix           = "${var.cluster_name}"
